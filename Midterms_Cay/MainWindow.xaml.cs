3613d498a713308265240a3102e6ab52c3f8990c
cay-t <25-006s@sgen.edu.ph>
2026-05-18 18:48:37 +0800

    Add project files.

---
+++ Midterms_Cay/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Midterms_Cay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Products = new List<Product> { };
+            AddedCart = new List<CartItem> { };
+            this.DataContext = this;
+        }
+        public List<Product> Products { get; }
+        public List<CartItem> AddedCart { get; }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+        }
+        public class CartItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (NameTextBox.Text != "" && TitleTextBox.Text != "" && SalaryTextBox.Text != "")
+                {
+                    Products.Add(new Product
+                    {
+                        Id = int.Parse(IdTextBox.Text),
+                        Name = NameTextBox.Text,
+                        Description = TitleTextBox.Text,
+                        Price = decimal.Parse(SalaryTextBox.Text)
+                    });
+                    myDataGrid.Items.Refresh();
+                    IdTextBox.Text = "";
+                    NameTextBox.Text = "";
+                    TitleTextBox.Text = "";
+                    SalaryTextBox.Text = "";
+
+                }
+                else
+                {
+                    MessageBox.Show("Please fill in all fields!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please fill in all fields correctly!");
+            }
+        }
+
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Product selectedProduct = myDataGrid.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+                Products.Remove(selectedProduct);
+                myDataGrid.Items.Refresh();
+                MessageBox.Show("Product removed successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Please select an product to remove!");
+            }
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (myDataGrid.SelectedItem is Product selectedProduct)
+            {
+                
+                try
+                {
+                    selectedProduct.Id = int.Parse(IdTextBox.Text);
+                    selectedProduct.Name = NameTextBox.Text;
+                    selectedProduct.Description = TitleTextBox.Text;
+                    selectedProduct.Price = decimal.Parse(SalaryTextBox.Text);
+                    myDataGrid.Items.Refresh();
+                }
+                catch
+                {
+                    MessageBox.Show("Please fill in all fields correctly!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a product to update!");
+            }
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (myDataGrid.SelectedItem is Product selectedProduct)
+            {
+                Products.Remove(selectedProduct);
+                myDataGrid.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Please select a product to remove!");
+            }
+        }
+
+        private void AddtoCartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Product selectedProduct = myDataGrid.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+                AddedCart.Add(new CartItem);
+                   
+                myDataGrid.Items.Refresh();
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a product to add in the cart!");
+            }
+        }
+    }
+}
